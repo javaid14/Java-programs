0265d779f66e5ef1670f8388c2c5d60e035ab500
javaid14 <91425137+javaid14@users.noreply.github.com>
2022-08-14 23:13:41 +0530

    Add files via upload

---
+++ Descending.java
@@ -0,0 +1,13 @@
+// Java program to sort the elements in descending order
+import java.util.*;
+
+class Descending {
+	public static void main(String[] args)
+	{
+		Integer array[] = { 1, 2, 3, 4, 5 };
+
+		Arrays.sort(array, Collections.reverseOrder());
+
+		System.out.println(Arrays.toString(array));
+	}
+}
