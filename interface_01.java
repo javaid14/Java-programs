2be22ec5b6026e346c28003bc5e4b49b595ffb46
javaid14 <91425137+javaid14@users.noreply.github.com>
2022-08-31 22:45:45 +0530

    Add files via upload

---
+++ interface_01.java
@@ -0,0 +1,28 @@
+package com.practiceeveryday;
+import java.util.*;
+class Test
+{
+    protected interface Yes
+    {
+        void show();
+    }
+}
+class Testing implements Test.Yes
+{
+    public void show()
+    {
+        System.out.println("show method of interface");
+    }
+}
+
+public class interface_01 {
+    public static void main(String[] args)
+    {
+        Test.Yes obj;
+        Testing t = new Testing();
+        obj=t;
+        obj.show();
+    }
+
+
+}
